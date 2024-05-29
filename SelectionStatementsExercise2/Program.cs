59dc08d2e89dc1eb86e1f70fba647f9f386768f5
Elsa Villarreal <elsa22@att.net>
2024-05-28 21:33:03 -0500

    exercise 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace SelectionStatementsExercise2
-{
+   {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your favorite school subject?");
+            string userInput = Console.ReadLine();
+            
+            switch (userInput.ToLower())
+            {
+                case "reading":
+                    Console.WriteLine($"{userInput}? Ahh, a bookworm?");
+                    break;
+
+                case "science":
+                    Console.WriteLine($"{userInput}? To infinity and beyond!");
+                    break;
+
+                case "math":
+                    Console.WriteLine($"{userInput}? Please excuse my dear Aunt Sally");
+                    break;
+
+                case "history":
+                    Console.WriteLine($"{userInput}? I remember a time when ...zzzz");
+                    break;
+
+                case "art":
+                    Console.WriteLine($"{userInput}? Yellow and blue make green?");
+                    break;
+
+                default:
+                    Console.WriteLine($"{userInput}, is that something new?");
+                    break;
+                    
+
+
+            }
         }
     }
 }
