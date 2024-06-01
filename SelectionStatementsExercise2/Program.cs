843b23b05a305cff7d759db0ef809d45c0fb4f98
Elsa Villarreal <elsa22@att.net>
2024-06-01 13:50:31 -0500

    1 and 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -5,32 +5,32 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, what is your favorite school subject?");
-            string userInput = Console.ReadLine();
+            string userSubject = Console.ReadLine();
             
-            switch (userInput.ToLower())
+            switch (userSubject.ToLower())
             {
                 case "reading":
-                    Console.WriteLine($"{userInput}? Ahh, a bookworm?");
+                    Console.WriteLine($"{userSubject}? Ahh, a bookworm?");
                     break;
 
                 case "science":
-                    Console.WriteLine($"{userInput}? To infinity and beyond!");
+                    Console.WriteLine($"{userSubject}? To infinity and beyond!");
                     break;
 
                 case "math":
-                    Console.WriteLine($"{userInput}? Please excuse my dear Aunt Sally");
+                    Console.WriteLine($"{userSubject}? Please excuse my dear Aunt Sally");
                     break;
 
                 case "history":
-                    Console.WriteLine($"{userInput}? I remember a time when ...zzzz");
+                    Console.WriteLine($"{userSubject}? I remember a time when ...zzzz");
                     break;
 
                 case "art":
-                    Console.WriteLine($"{userInput}? Yellow and blue make green?");
+                    Console.WriteLine($"{userSubject}? Yellow and blue make green?");
                     break;
 
                 default:
-                    Console.WriteLine($"{userInput}, is that something new?");
+                    Console.WriteLine($"{userSubject}, is that something new?");
                     break;
                     
 
--- SelectionStatementsExercise2/obj/Debug/net6.0/SelectionStatementsExercise2.AssemblyInfo.cs
+++ SelectionStatementsExercise2/obj/Debug/net6.0/SelectionStatementsExercise2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4142f11c4b5eb995429ab84b1b599ea5c4feaf49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f74e1428616a71eac4a14aa5a4425a83d5745d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
